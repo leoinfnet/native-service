b5282bad8090f25db64e3b6f2594b8d5f05192d1
leonardogloria <contato@leonardogloria.com.br>
2023-12-06 18:44:40 -0300

    Mudanca

--- src/main/java/org/acme/nativeservice/controller/HelloController.java
+++ src/main/java/org/acme/nativeservice/controller/HelloController.java
@@ -5,17 +5,21 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Response;
 import org.acme.nativeservice.model.ResponsePayload;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.LocalDateTime;
 
 @Path("/")
 public class HelloController {
+    Logger logger = LoggerFactory.getLogger(HelloController.class);
     @GET
     public Response greeting(){
         ResponsePayload responsePayload = new ResponsePayload();
         Faker faker = new Faker();
         responsePayload.message = faker.beer().name();
         responsePayload.dataHora = LocalDateTime.now();
+        logger.info(responsePayload.toString());
         return Response.ok().entity(responsePayload).build();
     }
 }
--- src/main/java/org/acme/nativeservice/model/ResponsePayload.java
+++ src/main/java/org/acme/nativeservice/model/ResponsePayload.java
@@ -6,4 +6,12 @@ public class ResponsePayload {
     public String message;
     public LocalDateTime dataHora;
 
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("ResponsePayload{");
+        sb.append("message='").append(message).append('\'');
+        sb.append(", dataHora=").append(dataHora);
+        sb.append('}');
+        return sb.toString();
+    }
 }
